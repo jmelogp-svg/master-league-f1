431d7fb2b0055592484d365c68fb06a75cae53a4
jmelogp-svg <jmelogp@gmail.com>
2025-12-10 15:20:43 -0300

    Versão Base Editável

--- master-league-f1/src/App.jsx
+++ master-league-f1/src/App.jsx
@@ -20,7 +20,6 @@ import ConsultarAnalises from './pages/ConsultarAnalises';
 import FormularioAcusacao from './pages/FormularioAcusacao';
 import FormularioDefesa from './pages/FormularioDefesa';
 import PainelVeredito from './pages/PainelVeredito';
-import Minicup from './pages/Minicup';
 
 function App() {
     return (
@@ -44,7 +43,6 @@ function App() {
                 <Route path="/acusacao" element={<FormularioAcusacao />} />
                 <Route path="/defesa" element={<FormularioDefesa />} />
                 <Route path="/veredito" element={<PainelVeredito />} />
-                <Route path="/minicup" element={<Minicup />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
